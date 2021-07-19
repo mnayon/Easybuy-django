b717b2bf50c6bd74743868187ceab7d461be0129
nayon <moniruzzamannayon96@gmail.com>
2021-07-20 01:25:55 +0600

    Registrations and login

--- easybuy/settings.py
+++ easybuy/settings.py
@@ -134,3 +134,17 @@ STATICFILES_DIRS = [
 #media file configarations
 MEDIS_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+   
+}
+
+#smtp configarations
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'moniruzzamannayon97@gmail.com'
+EMAIL_HOST_PASSWORD = '62966nayon62966'
+EMAIL_USE_TLS = True
